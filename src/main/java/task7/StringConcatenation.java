b034f415b5d0fe8de6542d0b5cce266a9c468409
Your Name <you@example.com>
2023-10-15 20:09:37 +0300

    Seminar2

---
+++ src/main/java/task7/StringConcatenation.java
@@ -0,0 +1,9 @@
+package task7;
+
+public class StringConcatenation {
+
+        public static String concatenateStrings(String str1, String str2) {
+            return str1 + " " + str2;
+        }
+    }
+
